ef576a56e3be82e54c64b46bde473cdf68d1f8ad
メタロロ <129175894+dmmo-com-jp@users.noreply.github.com>
2025-09-10 23:44:26 +0900

    Add files via upload

---
+++ CustomSelector.cs
@@ -0,0 +1,182 @@
+﻿using System.Collections.Immutable;
+using System.ComponentModel.DataAnnotations;
+using YukkuriMovieMaker.Commons;
+using YukkuriMovieMaker.Controls;
+using YukkuriMovieMaker.Exo;
+using YukkuriMovieMaker.Player.Video;
+using YukkuriMovieMaker.Plugin;
+using YukkuriMovieMaker.Plugin.Effects;
+
+namespace CustomSelector
+{
+    [VideoEffect("カスタムエフェクトセレクター", ["アニメーション"], [],true,false)]
+    internal class CustomSelector : VideoEffectBase
+    {
+        public override string Label => "カスタムエフェクトセレクター";
+        [Display(GroupName = "子映像エフェクト", Name = "", Description = "")]
+        [VideoEffectSelector(PropertyEditorSize = PropertyEditorSize.FullWidth)]
+        public ImmutableList<IVideoEffect> Effects { get => effects; set => Set(ref effects, value); }
+        ImmutableList<IVideoEffect> effects = [];
+
+        [Display(GroupName = "引数一覧", Name = "Value0", Description = "引数0")]
+        [AnimationSlider("F0", "", -100, 100)]
+        public Animation value0 { get; } = new Animation(0, -10000, 10000);
+
+        [Display(GroupName = "引数一覧", Name = "Value1", Description = "引数1")]
+        [AnimationSlider("F0", "", -100, 100)]
+        public Animation value1 { get; } = new Animation(0, -10000, 10000);
+
+        [Display(GroupName = "引数一覧", Name = "Value2", Description = "引数2")]
+        [AnimationSlider("F0", "", -100, 100)]
+        public Animation value2 { get; } = new Animation(0, -10000, 10000);
+
+        [Display(GroupName = "引数一覧", Name = "Value3", Description = "引数3")]
+        [AnimationSlider("F0", "", -100, 100)]
+        public Animation value3 { get; } = new Animation(0, -10000, 10000);
+
+        [Display(GroupName = "引数一覧", Name = "Value4", Description = "引数4")]
+        [AnimationSlider("F0", "", -100, 100)]
+        public Animation value4 { get; } = new Animation(0, -10000, 10000);
+
+        [Display(GroupName = "引数一覧", Name = "Value5", Description = "引数5")]
+        [AnimationSlider("F0", "", -100, 100)]
+        public Animation value5 { get; } = new Animation(0, -10000, 10000);
+
+        [Display(GroupName = "引数一覧", Name = "Value6", Description = "引数6")]
+        [AnimationSlider("F0", "", -100, 100)]
+        public Animation value6 { get; } = new Animation(0, -10000, 10000);
+
+        [Display(GroupName = "引数一覧", Name = "Value7", Description = "引数7")]
+        [AnimationSlider("F0", "", -100, 100)]
+        public Animation value7 { get; } = new Animation(0, -10000, 10000);
+
+        [Display(GroupName = "引数一覧", Name = "Value8", Description = "引数8")]
+        [AnimationSlider("F0", "", -100, 100)]
+        public Animation value8 { get; } = new Animation(0, -10000, 10000);
+
+        [Display(GroupName = "引数一覧", Name = "Value9", Description = "引数9")]
+        [AnimationSlider("F0", "", -100, 100)]
+        public Animation value9 { get; } = new Animation(0, -10000, 10000);
+
+
+        [Display(GroupName = "EffectDescription", Name = "Frame", Description = "Frameの式")]
+        [TextEditor(AcceptsReturn = true)]
+        public string frameformula { get => text; set => Set(ref text, value); }
+        string text = "frame";
+
+        [Display(GroupName = "EffectDescription", Name = "Length", Description = "Lengthの式")]
+        [TextEditor(AcceptsReturn = true)]
+        public string lenghtformula { get => text2; set => Set(ref text2, value); }
+        string text2= "length";
+
+        [Display(GroupName = "EffectDescription", Name = "Layer", Description = "Layerの式")]
+        [TextEditor(AcceptsReturn = true)]
+        public string layerformula { get => text3; set => Set(ref text3, value); }
+        string text3 = "layer";
+
+        [Display(GroupName = "EffectDescription", Name = "InputIndex", Description = "何文字目の式")]
+        [TextEditor(AcceptsReturn = true)]
+        public string inputindexformula { get => text4; set => Set(ref text4, value); }
+        string text4 = "InputIndex";
+
+        [Display(GroupName = "EffectDescription", Name = "InputCount", Description = "文字数の式")]
+        [TextEditor(AcceptsReturn = true)]
+        public string inputCountformula { get => text5; set => Set(ref text5, value); }
+        string text5 = "InputCount";
+
+        [Display(GroupName = "EffectDescription", Name = "GroupIndex", Description = "グループ制御の何番目かの式")]
+        [TextEditor(AcceptsReturn = true)]
+        public string groupindexformula { get => text6; set => Set(ref text6, value); }
+        string text6 = "GroupIndex";
+
+        [Display(GroupName = "EffectDescription", Name = "GroupCount", Description = "グループ制御の大きさの式")]
+        [TextEditor(AcceptsReturn = true)]
+        public string groupCountformula { get => text7; set => Set(ref text7, value); }
+        string text7 = "GroupCount";
+
+
+        [Display(GroupName = "DrawDescription", Name = "X座標", Description = "X座標の式")]
+        [TextEditor(AcceptsReturn = true)]
+        public string Xformula { get => tx1; set => Set(ref tx1, value); }
+        string tx1 = "X";
+
+        [Display(GroupName = "DrawDescription", Name = "Y座標", Description = "Y座標の式")]
+        [TextEditor(AcceptsReturn = true)]
+        public string Yformula { get => tx2; set => Set(ref tx2, value); }
+        string tx2 = "Y";
+
+        [Display(GroupName = "DrawDescription", Name = "Z座標", Description = "Z座標の式")]
+        [TextEditor(AcceptsReturn = true)]
+        public string Zformula { get => tx3; set => Set(ref tx3, value); }
+        string tx3 = "Z";
+
+        [Display(GroupName = "DrawDescription", Name = "ZoomX", Description = "ZoomXの式")]
+        [TextEditor(AcceptsReturn = true)]
+        public string ZoomXformula { get => tx4; set => Set(ref tx4, value); }
+        string tx4 = "ZoomX";
+
+        [Display(GroupName = "DrawDescription", Name = "ZoomY", Description = "ZoomYの式")]
+        [TextEditor(AcceptsReturn = true)]
+        public string ZoomYformula { get => tx5; set => Set(ref tx5, value); }
+        string tx5 = "ZoomY";
+        //Rotation
+        [Display(GroupName = "DrawDescription", Name = "X回転", Description = "X回転の式")]
+        [TextEditor(AcceptsReturn = true)]
+        public string RotationXformula { get => tx6; set => Set(ref tx6, value); }
+        string tx6 = "RotationX";
+
+        [Display(GroupName = "DrawDescription", Name = "Y回転", Description = "Y回転の式")]
+        [TextEditor(AcceptsReturn = true)]
+        public string RotationYformula { get => tx7; set => Set(ref tx7, value); }
+        string tx7 = "RotationY";
+
+        [Display(GroupName = "DrawDescription", Name = "Z回転", Description = "Z回転の式")]
+        [TextEditor(AcceptsReturn = true)]
+        public string RotationZformula { get => tx8; set => Set(ref tx8, value); }
+        string tx8 = "RotationZ";
+
+
+        [Display(GroupName = "変数設定", Name = "", Description = "")]
+        [TextEditor(AcceptsReturn = true)]
+        public string VarList { get => vars; set => Set(ref vars, value); }
+        string vars = "";
+
+        [Display(GroupName = "実行条件", Name = "", Description = "")]
+        [TextEditor(AcceptsReturn = true)]
+        public string booleanClac { get => boolclac; set => Set(ref boolclac, value); }
+        string boolclac = "true";
+
+
+        public override IEnumerable<string> CreateExoVideoFilters(int keyFrameIndex, ExoOutputDescription exoOutputDescription)
+        {
+            //サンプルはSampleD2DVideoEffectを参照
+            return [];
+        }
+
+        /// <summary>
+        /// 映像エフェクトを作成する
+        /// </summary>
+        /// <param name="devices">デバイス</param>
+        /// <returns>映像エフェクト</returns>
+        public override IVideoEffectProcessor CreateVideoEffect(IGraphicsDevicesAndContext devices)
+        {
+            return new CustomSelectorProcessor(this, devices);
+        }
+
+        /// <summary>
+        /// クラス内のIAnimatableを列挙する。
+        /// </summary>
+        /// <returns></returns>
+        protected override IEnumerable<IAnimatable> GetAnimatables() => [
+            value0,value1,value2,value3,value4,value5,value6,value7,value8,value9,
+            .. effects
+            ];
+        public PluginDetailsAttribute Details => new()
+        {
+            //制作者
+            AuthorName = "メタロロ",
+            //作品ID
+            ContentId = "",
+        };
+    }
+}
---
+++ CustomSelectorProcessor.cs
@@ -0,0 +1,200 @@
+﻿using NCalc;
+using Vortice.Direct2D1;
+using YukkuriMovieMaker.Commons;
+using YukkuriMovieMaker.Player.Video;
+using YukkuriMovieMaker.Plugin.Effects;
+
+
+namespace CustomSelector
+{
+    internal class CustomSelectorProcessor : IVideoEffectProcessor
+    {
+        readonly IGraphicsDevicesAndContext devices;
+        readonly CustomSelector item;
+        ID2D1Image? input;
+        ID2D1Image? output;
+        public List<IVideoEffectProcessor> videoEffectProcessors = new List<IVideoEffectProcessor>();
+        public System.Collections.Immutable.ImmutableList<IVideoEffect> childeffects;
+        public ID2D1Image Output => output ?? input ?? throw new NullReferenceException("No valid output image");
+        public CustomSelectorProcessor(CustomSelector item, IGraphicsDevicesAndContext devices)
+        {
+
+            this.item = item;
+            this.devices = devices;
+            for (int i = 0; i < item.Effects.Count; i++)
+            {
+                videoEffectProcessors.Add(item.Effects[i].CreateVideoEffect(devices));
+
+            }
+            childeffects = item.Effects;
+        }
+        private object doclac(string clactxt,EffectDescription effectDescription ,Dictionary<string,object?>Paramlist)
+        {
+            var frame = effectDescription.ItemPosition.Frame;
+            var length = effectDescription.ItemDuration.Frame;
+            var fps = effectDescription.FPS;
+            var layer = effectDescription.Layer;
+            var inputIndex = effectDescription.InputIndex;
+            var inputCount = effectDescription.InputCount;
+            var groupIndex = effectDescription.GroupIndex;
+            var groupCount = effectDescription.GroupCount;
+            var drawDesc = effectDescription.DrawDescription;
+            try
+            {
+                var cfreme = new Expression(clactxt);
+                cfreme.Parameters = Paramlist;
+                cfreme.Parameters["frame"] = frame;
+                cfreme.Parameters["length"] = length;
+                cfreme.Parameters["fps"] = fps;
+                cfreme.Parameters["layer"] = layer;
+                cfreme.Parameters["InputIndex"] = inputIndex;
+                cfreme.Parameters["InputCount"] = inputCount;
+                cfreme.Parameters["GroupIndex"] = groupIndex;
+                cfreme.Parameters["GroupCount"] = groupCount;
+
+                cfreme.Parameters["X"] = drawDesc.Draw.X;
+                cfreme.Parameters["Y"] = drawDesc.Draw.Y;
+                cfreme.Parameters["Z"] = drawDesc.Draw.Z;
+                cfreme.Parameters["ZoomX"] = drawDesc.Zoom.X;
+                cfreme.Parameters["ZoomY"] = drawDesc.Zoom.Y;
+                cfreme.Parameters["RotationX"] = drawDesc.Rotation.X;
+                cfreme.Parameters["RotationY"] = drawDesc.Rotation.Y;
+                cfreme.Parameters["RotationZ"] = drawDesc.Rotation.Z;
+                cfreme.Parameters["Opacity"] = drawDesc.Opacity;
+
+                cfreme.Parameters["Value0"] = item.value0.GetValue(frame, length, fps);
+                cfreme.Parameters["Value1"] = item.value1.GetValue(frame, length, fps);
+                cfreme.Parameters["Value2"] = item.value2.GetValue(frame, length, fps);
+                cfreme.Parameters["Value3"] = item.value3.GetValue(frame, length, fps);
+                cfreme.Parameters["Value4"] = item.value4.GetValue(frame, length, fps);
+                cfreme.Parameters["Value5"] = item.value5.GetValue(frame, length, fps);
+                cfreme.Parameters["Value6"] = item.value6.GetValue(frame, length, fps);
+                cfreme.Parameters["Value7"] = item.value7.GetValue(frame, length, fps);
+                cfreme.Parameters["Value8"] = item.value8.GetValue(frame, length, fps);
+                cfreme.Parameters["Value9"] = item.value9.GetValue(frame, length, fps);
+                var res = cfreme.Evaluate();
+                return res;
+            }
+            catch (Exception ex)
+            {
+                // その他の予期せぬエラーも捕捉する
+                Console.WriteLine($"予期せぬエラー: {ex.Message}");
+            }
+            return 0;
+        }
+        /// <summary>
+        /// 数値の入ったobject型をint型に変換するやつ
+        /// 表現不可能は0になる
+        /// </summary>
+        /// <param name="value"></param>
+        private int Convertint(object value) 
+        {
+            try
+            {
+                return Convert.ToInt32(value);
+            }
+            catch (Exception e)
+            {
+                return 0;
+            }
+        }
+        private float Convertfloat(object value)
+        {
+            try
+            {
+                return Convert.ToSingle(value);
+            }
+            catch (Exception e)
+            {
+                return 0;
+            }
+        }
+        public DrawDescription Update(EffectDescription effectDescription)
+        {
+            //こいつ入れないと子エフェクトの追加削除等の反映が遅れる。
+            if(!childeffects.SequenceEqual(item.Effects))
+            {
+                videoEffectProcessors = [];
+                for (int i = 0; i < item.Effects.Count; i++)
+                {
+                    videoEffectProcessors.Add(item.Effects[i].CreateVideoEffect(devices));
+
+                }
+                childeffects = item.Effects;
+            }
+            var frame = effectDescription.ItemPosition.Frame;
+            var length = effectDescription.ItemDuration.Frame;
+            var fps = effectDescription.FPS;
+            var Params = item.VarList.Replace(Environment.NewLine, "").Split(',');
+            var paramlist = new Dictionary<string, object?>();
+            for(int i=0;i<Params.Length;i++)
+            {
+                var param = Params[i].Replace("==","::").Replace("!=", "!:").Replace("<=", "<:").Replace(">=", ">:").Split("=");
+                try
+                {
+                    paramlist[param[0]] = doclac(param[1].Replace(":","="), effectDescription, paramlist);
+                }
+                catch(Exception e) { }
+            }
+            int rframe = Convertint(doclac(item.frameformula, effectDescription, paramlist));
+            int rlength = Convertint(doclac(item.lenghtformula, effectDescription, paramlist));
+            int rlayer = Convertint(doclac(item.layerformula, effectDescription, paramlist));
+            int rinputindex = Convertint(doclac(item.inputindexformula, effectDescription, paramlist));
+            int rinputcount = Convertint(doclac(item.inputCountformula, effectDescription, paramlist));
+            int rgroupindex = Convertint(doclac(item.groupindexformula, effectDescription, paramlist));
+            int rgroupcount = Convertint(doclac(item.groupCountformula, effectDescription, paramlist));
+
+            object CanDoEffect = doclac(item.booleanClac, effectDescription, paramlist);
+            Console.WriteLine(CanDoEffect.GetType());
+            output = input;
+            if (CanDoEffect is Boolean && (bool)CanDoEffect == false) return effectDescription.DrawDescription;
+            var tmpdraw = effectDescription.DrawDescription;
+            for(int i = 0; i < videoEffectProcessors.Count; i++)
+            {
+                var Timelinedesc = new TimelineItemSourceDescription(effectDescription, rframe,rlength, rlayer);
+                var effectdesc = new EffectDescription(Timelinedesc, tmpdraw, rinputindex, rinputcount, rgroupindex, rgroupcount);
+                videoEffectProcessors[i].SetInput(output);
+                tmpdraw =videoEffectProcessors[i].Update(effectdesc);
+                output = videoEffectProcessors[i].Output;
+            }
+            {
+                var effectdesc = new EffectDescription(effectDescription, tmpdraw, rinputindex, rinputcount, rgroupindex, rgroupcount);
+                tmpdraw = indraw(paramlist, effectdesc);
+            }
+            return tmpdraw;
+        }
+        private DrawDescription indraw(Dictionary<string,object?> paramlist,EffectDescription effectDescription) 
+        {
+            return effectDescription.DrawDescription with
+            {
+                Draw = new(
+                    Convertfloat(doclac(item.Xformula, effectDescription, paramlist)),
+                    Convertfloat(doclac(item.Yformula, effectDescription, paramlist)),
+                    Convertfloat(doclac(item.Zformula, effectDescription, paramlist))
+                    ),
+                Rotation = new(
+                    Convertfloat(doclac(item.RotationXformula, effectDescription, paramlist)),
+                    Convertfloat(doclac(item.RotationYformula, effectDescription, paramlist)),
+                    Convertfloat(doclac(item.RotationZformula, effectDescription, paramlist))
+                    ),
+                Zoom = new(
+                    Convertfloat(doclac(item.ZoomXformula, effectDescription, paramlist)),
+                    Convertfloat(doclac(item.ZoomYformula, effectDescription, paramlist))
+                    ),
+
+            };
+        }
+        public void ClearInput()
+        {
+            for (int i = 0; i < videoEffectProcessors.Count; i++) { videoEffectProcessors[i].ClearInput(); }
+            input = null;
+        }
+        public void SetInput(ID2D1Image? input)
+        {
+            this.input = input;
+        }
+        public void Dispose() {
+            for(int i = 0;i < videoEffectProcessors.Count;i++) { videoEffectProcessors[i].Dispose(); }
+        }
+    }
+}
